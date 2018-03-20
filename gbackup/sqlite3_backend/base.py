62d5d2720eadb944597bdac9e1fb94466bdd7a1c
Andrew Brown <brownan@gmail.com>
2018-03-20 12:17:50 -0400

    comments in sqlite backend

--- gbackup/sqlite3_backend/base.py
+++ gbackup/sqlite3_backend/base.py
@@ -2,21 +2,16 @@
 This custom database backend inherits from the sqlite3 backend but adds
 a few customizations for this application.
 
-In particular, it adds:
-* Cascading deletes to foreign key relations. Foreign key fields with on_delete
-  set to DO_NOTHING will efficiently delete all objects referencing the deleted
-  object without pulling them into memory. Signals and such aren't run, but for
-  large tables, this is the only way to delete a large number of objects
-  without blowing up memory usage and taking forever.
-* Uses the Write-ahead Log journal mode to support readers reading
-  simultaneously with a single writer.
-* Supports starting transactions with BEGIN IMMEDIATE for immediately
-  acquiring the RESERVED lock
-
 """
 from django.db.backends.sqlite3 import base, schema
 
 class DatabaseSchemaEditor(schema.DatabaseSchemaEditor):
+    # This adds cascading deletes to foreign key relations. Foreign key
+    # fields with on_delete set to DO_NOTHING will still cascade, but SQLite
+    # will do the cascading, without Django having to pull all rows into
+    # memory. Signals and such aren't run, but for large tables, this is the
+    # only way to delete a large number of objects without using a lot of
+    # memory.
     sql_create_inline_fk = "REFERENCES %(to_table)s (%(to_column)s) " \
                            "ON DELETE CASCADE " \
                            "DEFERRABLE INITIALLY DEFERRED"
@@ -25,25 +20,40 @@ class DatabaseWrapper(base.DatabaseWrapper):
     SchemaEditorClass = DatabaseSchemaEditor
 
     def __init__(self, *args, **kwargs):
-        # Set by a custom context manager to tell us to use BEGIN IMMEDIATE
-        # when beginning a transaction
+        # This flag is set by a custom context manager to tell us to use BEGIN
+        # IMMEDIATE when beginning a transaction
         self.begin_immediate = False
         super().__init__(*args, **kwargs)
 
+    def _start_transaction_under_autocommit(self):
+        if self.begin_immediate:
+            self.cursor().execute("BEGIN IMMEDIATE")
+        else:
+            self.cursor().execute("BEGIN")
+
     def get_new_connection(self, conn_params):
-        """Enable a couple sqlite features that are disabled by default"""
+        """Enable some sqlite features that are disabled by the default"""
         conn = super().get_new_connection(conn_params)
 
-        # The write-ahead-log doesn't add much performance to our use case.
-        # The main advantage is that it allows readers while another
-        # connection is in a write transaction. Since there's typically just
-        # one process accessing the database, this is mainly useful for
-        # debugging: we can access the database while a scan or some other
-        # big operation is running
+        # The write-ahead-log's main advantage is that it allows readers
+        # while another connection is in a write transaction. It also may add
+        # some performance improvements, but at the moment, tests show
+        # performance is about the same.
         conn.execute("PRAGMA journal_mode=WAL").close()
 
-        # The .close() fixes the cursor being left open causing SQLITE_BUSY
-        # on later commits. This is only a problem on PyPy.
+        # The next two options can help improve performance for some kinds of
+        # workloads, but according to some quick tests, performance is about
+        # the same for this application. My guess is that performance is
+        # limited by other IO and the database is not the main bottleneck.
+        # I'm leaving these settings in the code but commented out so it's
+        # easy to uncomment them for performance tests in the future.
+
+        # Setting the synchronous mode to NORMAL sacrifices durability for
+        # performance. Writes to the WAL are not synced, so writes may be lost
+        # on a system crash or power failure, which feels acceptable for this
+        # application. Writes to the database are still synced, and the WAL
+        # is still synced before checkpoints.
+        #conn.execute("PRAGMA synchronous=NORMAL").close()
 
         # Memory-mapped IO can help performance on read-heavy loads by
         # avoiding a lot of read() system calls, but according to some quick
@@ -56,9 +66,3 @@ class DatabaseWrapper(base.DatabaseWrapper):
         # more memory than I expect.
         #conn.execute("PRAGMA mmap_size=1073741824;").close()
         return conn
-
-    def _start_transaction_under_autocommit(self):
-        if self.begin_immediate:
-            self.cursor().execute("BEGIN IMMEDIATE")
-        else:
-            self.cursor().execute("BEGIN")
