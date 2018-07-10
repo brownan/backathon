229665a210c94eb3f3e744bc43006522dec2113d
Andrew Brown <brownan@gmail.com>
2018-07-09 21:35:22 -0400

    fixed a docstring

--- backathon/dbrouter.py
+++ backathon/dbrouter.py
@@ -1,9 +1,9 @@
 class BackathonRouter:
-    """Makes sure we don't apply backathon migrations to the default router
+    """Makes sure we don't apply backathon migrations to the default database
 
     Our models should always go into a database managed by the Repository
-    class. This stops the default migrate command from trying to create our
-    tables in the default database.
+    class. This router stops the default migrate command from trying to
+    create our tables in the default database.
 
     Later we may want to have some models in the default database and others
     in the repository-specific databases. That logic goes in here.
