7edb229f6553937c71b8dd8132325c6ce28788bf
Andrew Brown <andrew@brownan.org>
2024-08-08 15:36:58 -0400

    Removed old django-related code

--- backathon/commands/listroots.py
+++
@@ -1,9 +0,0 @@
-from backathon.commands import CommandBase
-
-
-class Command(CommandBase):
-    help = "Lists currently defined backup roots"
-
-    def handle(self, *args, **kwargs):
-        for entry in self.get_repo().get_roots():
-            print(entry.printablepath)
