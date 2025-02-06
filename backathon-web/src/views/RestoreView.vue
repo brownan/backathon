07dbe5bbb6d95f0899ef5c0d9769b7b21639ca32
Andrew Brown <andrew@brownan.org>
2025-02-06 12:03:49 -0500

    RestoreView.vue

--- backathon-web/src/views/RestoreView.vue
+++ backathon-web/src/views/RestoreView.vue
@@ -1,4 +1,14 @@
-<template>restore view</template>
+<template>
+    restore view
+
+    <h2>Snapshots</h2>
+    <table>
+        <tr>
+            <th>Path</th>
+            <th>Timestamp</th>
+        </tr>
+    </table>
+</template>
 
 <style scoped></style>
 
