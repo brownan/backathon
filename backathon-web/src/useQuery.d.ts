6e6e8477aa72e398fe77aa2f54664eca61091e36
Andrew Brown <andrew@brownan.org>
2025-02-18 11:57:47 -0500

    removed unused types

--- backathon-web/src/useQuery.d.ts
+++ backathon-web/src/useQuery.d.ts
@@ -2,46 +2,12 @@ import type {
     ErrorResponse,
     HttpMethod,
     PathsWithMethod,
-    RequiredKeysOf,
     SuccessResponse,
 } from "openapi-typescript-helpers";
-import {
-    type BodySerializer,
-    type ClientOptions,
-    DefaultParamsOption,
-    type HeadersOptions,
-    type MaybeOptionalInit,
-    type ParseAs,
-    type QuerySerializer,
-    type QuerySerializerOptions,
-    type RequestBodyOption,
-} from "openapi-fetch";
+import { type MaybeOptionalInit } from "openapi-fetch";
 import type { paths } from "./schema.d.ts";
 
 import { type MaybeRefOrGetter } from "vue";
-
-type ReactiveKeyedObject<Obj> = { [K in keyof Obj]: MaybeRefOrGetter<Obj[K]> };
-
-type ReactiveParamsOption<T> = T extends {
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    parameters: any;
-}
-    ? RequiredKeysOf<T["parameters"]> extends never
-        ? { params?: ReactiveKeyedObject<T["parameters"]> }
-        : { params: ReactiveKeyedObject<T["parameters"]> }
-    : DefaultParamsOption;
-
-export type ReactiveFetchOptions<Operation> = {
-    enable?: MaybeRefOrGetter<boolean>;
-} & ReactiveParamsOption<Operation> &
-    RequestBodyOption<Operation> & {
-        baseUrl?: string;
-        querySerializer?: QuerySerializer<T> | QuerySerializerOptions;
-        bodySerializer?: BodySerializer<T>;
-        parseAs?: ParseAs;
-        fetch?: ClientOptions["fetch"];
-        headers?: HeadersOptions;
-    } & Omit<RequestInit, "body" | "headers">;
 
 export interface QueryState<T, E> {
     data: T | null;
