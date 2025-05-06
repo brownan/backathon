fc821bf2c4dd20c3a814858bb61010dd0fda285e
Andrew Brown <andrew@brownan.org>
2025-05-06 14:29:17 -0400

    schedule settings UI save and load

--- backathon-web/src/views/ConfigureSchedule.vue
+++ backathon-web/src/views/ConfigureSchedule.vue
@@ -9,7 +9,11 @@
                 <div class="field">
                     <div class="control">
                         <label class="checkbox">
-                            <input type="checkbox" />
+                            <input
+                                v-if="settings"
+                                type="checkbox"
+                                v-model="settings.enable"
+                            />
                             Enable Schedule
                         </label>
                     </div>
@@ -18,11 +22,15 @@
                     <label class="label">Schedule Type</label>
                     <div class="control">
                         <div class="select">
-                            <select>
-                                <option>Hourly</option>
-                                <option>Daily</option>
-                                <option>Weekly</option>
-                                <option>Monthly</option>
+                            <select
+                                v-if="settings"
+                                class="select"
+                                v-model="settings.mode"
+                            >
+                                <option value="hourly">Hourly</option>
+                                <option value="daily">Daily</option>
+                                <option value="weekly">Weekly</option>
+                                <option value="monthly">Monthly</option>
                             </select>
                         </div>
                     </div>
@@ -31,11 +39,22 @@
                     <label class="label">Next Backup At</label>
                     <div class="control">
                         <DatePicker
+                            v-if="settings"
                             hour-format="12"
                             show-time
                             fluid
+                            v-model="settings.nextRunTime"
                         />
                     </div>
+                </div>
+                <div class="field">
+                    <button
+                        type="button"
+                        class="button is-primary"
+                        @click="doUpdate"
+                    >
+                        Save
+                    </button>
                 </div>
             </div>
         </div>
@@ -50,4 +69,32 @@
 
 <script setup lang="ts">
 import DatePicker from "primevue/datepicker";
+import { useQuery, client } from "@/api.ts";
+import type { components } from "@/schema.d.ts";
+import { watch, ref } from "vue";
+
+const scheduleAPI = useQuery({
+    method: "get",
+    url: "/schedule",
+    options: {},
+});
+
+const settings = ref<components["schemas"]["ScheduleSettings"] | null>(null);
+
+watch(
+    () => scheduleAPI.data,
+    (val) => {
+        if (val) {
+            settings.value = val;
+        }
+    },
+);
+
+function doUpdate() {
+    if (settings.value) {
+        client.POST("/schedule", {
+            body: settings.value,
+        });
+    }
+}
 </script>
